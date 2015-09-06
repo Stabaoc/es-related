4c2d5b50970d64325034551b5d4f552e1eef8f12
stab <sohach@163.com>
2015-09-06 17:47:08 +0800

    change

--- src/main/java/edu/song/elasticsearch/search/SearchTest.java
+++ src/main/java/edu/song/elasticsearch/search/SearchTest.java
@@ -1,14 +1,23 @@
 package edu.song.elasticsearch.search;
 
+import org.apache.lucene.index.Term;
+import org.apache.lucene.queryparser.xml.FilterBuilder;
 import org.elasticsearch.action.search.SearchResponse;
-import org.elasticsearch.bootstrap.Bootstrap;
 import org.elasticsearch.client.Client;
+import org.elasticsearch.index.query.BoolFilterBuilder;
+import org.elasticsearch.index.query.ExistsFilterBuilder;
+import org.elasticsearch.index.query.FilterBuilders;
 import org.elasticsearch.index.query.QueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.index.query.RangeFilterBuilder;
+import org.elasticsearch.index.query.TermFilterBuilder;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.aggregations.AggregationBuilder;
 import org.elasticsearch.search.aggregations.AggregationBuilders;
-import org.elasticsearch.search.aggregations.bucket.histogram.Histogram.Order;
 import org.elasticsearch.search.aggregations.bucket.terms.Terms;
 import org.elasticsearch.search.aggregations.bucket.terms.Terms.Bucket;
+import org.elasticsearch.search.aggregations.metrics.cardinality.Cardinality;
+import org.elasticsearch.search.aggregations.metrics.cardinality.CardinalityBuilder;
 import org.elasticsearch.search.sort.SortBuilder;
 import org.elasticsearch.search.sort.SortBuilders;
 import org.elasticsearch.search.sort.SortOrder;
@@ -55,7 +64,77 @@ public class SearchTest {
 		}
 	}
 	
+	public static void termAgg(){
+		QueryBuilder query = QueryBuilders.termQuery("serverUrl", "/jeeshopclient/user/editAddress.html");
+		AggregationBuilder<?> termAgg = AggregationBuilders.terms("serviceIds").field("messageType");
+		
+		SearchResponse searchResponse = client.prepareSearch("apm_121.40.159.7")
+				.setTypes("JavaAppData")
+				.setQuery(query)
+				.addAggregation(termAgg)
+				.execute().actionGet();
+		
+		System.out.println(searchResponse);
+	}
+	
+	public static void getMessageId() {
+		TermFilterBuilder messageIdFilter = FilterBuilders.termFilter("messageId", 0);
+		RangeFilterBuilder tiemFilter = FilterBuilders.rangeFilter("startTime").from("2015-09-06 10:00:00");
+		BoolFilterBuilder boolFilter = FilterBuilders.boolFilter().mustNot(messageIdFilter).must(tiemFilter);
+		QueryBuilder query = QueryBuilders.filteredQuery(null, boolFilter);
+		AggregationBuilder<?> termAgg = AggregationBuilders.terms("transactionIds").field("transactionId").size(0)
+				.subAggregation(AggregationBuilders.terms("spanIds").field("spanId")
+						.subAggregation(AggregationBuilders.terms("messageIds").field("messageId")));
+		
+		SearchResponse searchResponse = client.prepareSearch("apm_121.40.159.7")
+				.setTypes("JavaAppData")
+				.setQuery(query)
+				.addAggregation(termAgg)
+				.execute().actionGet();
+		
+		Terms transactionIds = searchResponse.getAggregations().get("transactionIds");
+		for(Bucket transactionIdBucket:transactionIds.getBuckets()){
+			Terms spanIds = transactionIdBucket.getAggregations().get("spanIds");
+			for(Bucket spanIdBucket:spanIds.getBuckets()){
+				Terms messageIds = spanIdBucket.getAggregations().get("messageIds");
+				for(Bucket messageIdBucket:messageIds.getBuckets()){
+					if(messageIdBucket.getDocCount() > 1)
+						System.out.println("Error:" + "transactionId: " + transactionIdBucket.getKey() + 
+								",spanId: " + spanIdBucket.getKey() + "  " + spanIdBucket.getDocCount() + "  " +
+								messageIdBucket.getDocCount());
+//					else
+//						System.out.println("  " + "transactionId: " + transactionIdBucket.getKey() + 
+//								",spanId: " + spanIdBucket.getKey() + "  " + spanIdBucket.getDocCount() + "  " +
+//								messageIdBucket.getDocCount());
+				}
+			}
+		}
+	}
+	
+	public static void searchDbGtTotal() {
+		ExistsFilterBuilder exist = FilterBuilders.existsFilter("timestamp");
+		QueryBuilder matchAll = QueryBuilders.matchAllQuery();
+		
+		QueryBuilder query = QueryBuilders.filteredQuery(matchAll, exist);
+		
+		SearchResponse searchResponse = client.prepareSearch("apm_121.40.159.7")
+				.setTypes("Response")
+				.setQuery(query)
+				.setSize(1000000)
+				.execute().actionGet();
+		
+		for(SearchHit hit:searchResponse.getHits()){
+			Number db = (Number) hit.getSource().get("dbTime");
+			Number total = (Number) hit.getSource().get("totalTime");
+			if(db.longValue() > total.longValue())
+				System.out.println(hit.getSource());
+		}
+	}
+	
 	public static void main(String[] args) {
-        Bootstrap.main(args);
-    }
+        //Bootstrap.main(args);
+		//termAgg();
+		//getMessageId();
+		searchDbGtTotal();
+	}
 }
